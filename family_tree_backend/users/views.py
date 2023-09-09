7183889efa058583bdbebcf5d4bb413ff13c5504
Nick <nodhiambo01@gmail.com>
2023-09-09 23:41:35 +0300

    Updates

--- family_tree_backend/users/views.py
+++ family_tree_backend/users/views.py
@@ -3,9 +3,8 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.decorators import login_required
-# Create your views here.
 
+# Create your views here.
 
 def admin_logout(request):
     """Logs out the admin"""
