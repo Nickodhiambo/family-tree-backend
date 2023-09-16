a351246f692ffa08500af7e54e3232c19acc30e7
Nick <nodhiambo01@gmail.com>
2023-09-16 09:11:41 +0300

    Updates

--- family_tree_backend/family_tree_backend/settings.py
+++ family_tree_backend/family_tree_backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t5!8_qsokq7qff1azzb+flul#8c!lhf3dwha^v)zyul1d4*3ww'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
