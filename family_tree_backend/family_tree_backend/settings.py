30b9c384d038a03989f4425aed684beda8cf8da7
Nick <nodhiambo01@gmail.com>
2023-09-23 16:25:43 +0300

    Latest update

--- family_tree_backend/family_tree_backend/settings.py
+++ family_tree_backend/family_tree_backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+#import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,20 +83,20 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-       # 'default': {
-           # 'ENGINE': 'django.db.backends.sqlite3',
-            #'NAME': BASE_DIR / 'sqlite3'
-            #}
-#}
-
 DATABASES = {
-        'default': dj_database_url.config (
-            default='postgresql://postgres:postgres@localhost:5432/family_tree_backend',
-            conn_max_age=600,
-            conn_health_checks=True,
-            )
-        }
+        'default': {
+           'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'sqlite3'
+            }
+}
+
+#DATABASES = {
+        #'default': dj_database_url.config (
+            #default='postgresql://postgres:postgres@localhost:5432/family_tree_backend',
+            #conn_max_age=600,
+            #conn_health_checks=True,
+            #)
+        #}
 
 
 # Password validation
