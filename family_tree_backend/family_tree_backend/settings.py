e02cfa8e965a4d3e8b2421bdb61380c566a1551b
Nick <nodhiambo01@gmail.com>
2023-11-19 10:20:01 +0300

    JWT settings

--- family_tree_backend/family_tree_backend/settings.py
+++ family_tree_backend/family_tree_backend/settings.py
@@ -157,8 +157,8 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-        'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-        'REFRESH_TOKEN_LIFETIME': timedelta(days=150),
+        'ACCESS_TOKEN_LIFETIME': timedelta(days=365),
+        'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
         }
 
 AUTHENTICATION_BACKENDS = [
