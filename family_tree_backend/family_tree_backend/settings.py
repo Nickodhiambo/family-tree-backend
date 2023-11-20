81ec82084706ab14cf0c2ae1ed011d6978219e3f
Nick <nodhiambo01@gmail.com>
2023-11-20 12:54:27 +0300

    Modified jwt settings

--- family_tree_backend/family_tree_backend/settings.py
+++ family_tree_backend/family_tree_backend/settings.py
@@ -157,8 +157,8 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-        'ACCESS_TOKEN_LIFETIME': timedelta(days=365),
-        'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
+        'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+        'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
         }
 
 AUTH_USER_MODEL = 'users.CustomUser'
