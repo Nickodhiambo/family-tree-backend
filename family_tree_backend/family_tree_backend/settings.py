da3dd2c37b905935bea4f8ec8937a4ac40b094e1
Nick <nodhiambo01@gmail.com>
2023-11-20 14:52:07 +0300

    Update

--- family_tree_backend/family_tree_backend/settings.py
+++ family_tree_backend/family_tree_backend/settings.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-        'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+        'ACCESS_TOKEN_LIFETIME': timedelta(minutes=4),
         'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
         }
 
